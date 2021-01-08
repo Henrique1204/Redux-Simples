4f6f3dd11174b36e243e2151195ca1b1aa9d4efe
Paulo <pauloh16.pdr@gmail.com>
2021-01-08 17:58:50 -0300

    Adicionar estrutura base para componente App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
+// Importando estilo.
 import "./App.css";
+// Importando componentes da interface.
+import Card from "./Componentes/Card/Card.jsx";
 
-function App() {
+const App = () => {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <h1>Exercício React-Redux (Simples)</h1>
+
+      <div className="linha">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
---
+++ src/Componentes/Card/Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// Importando estilos do componente.
+import estilos from "./Card.module.css";
+
+const Card = () => {
+    return (
+        <div className={estilos.Card}>
+            Card
+        </div>
+    );
+};
+
+export default Card;
