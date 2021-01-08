aa3187ebeb11e4f38c5eab0abe31b415858aff01
Paulo <pauloh16.pdr@gmail.com>
2021-01-08 18:26:41 -0300

    Adicionar estrutura no componente Card

--- src/App.js
+++ src/App.js
@@ -10,9 +10,17 @@ const App = () => {
       <h1>Exercício React-Redux (Simples)</h1>
 
       <div className="linha">
-        <Card />
-        <Card />
-        <Card />
+        <Card title="Card 1" color="purple" >A</Card>
+        <Card title="Card 2" color="blue" >B</Card>
+      </div>
+  
+      <div className="linha">
+        <Card title="Card 3" color="red" >C</Card>
+        <Card title="Card 4" color="green" >D</Card>
+      </div>
+
+      <div className="linha">
+        <Card title="Card 5" >E</Card>
       </div>
     </div>
   );
--- src/Componentes/Card/Card.jsx
+++ src/Componentes/Card/Card.jsx
@@ -2,10 +2,16 @@ import React from "react";
 // Importando estilos do componente.
 import estilos from "./Card.module.css";
 
-const Card = () => {
+const Card = ({ title, children, color }) => {
     return (
-        <div className={estilos.Card}>
-            Card
+        <div className={`${estilos.Card} ${estilos[color] || estilos.default}`}>
+            <div className={estilos.header}>
+                <span className={estilos.title} >{title}</span>
+            </div>
+
+            <div className={estilos.content}>
+                {children}
+            </div>
         </div>
     );
 };
