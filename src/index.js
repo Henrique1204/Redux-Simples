12ec6f06eee213c9a3d0b7920de9935789860dfc
Paulo <pauloh16.pdr@gmail.com>
2021-01-09 13:46:16 -0300

    Configurar store do Redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+// Importando componentes externos.
+import { Provider } from "react-redux";
+// Importando store.
+import store from "./store/storeConfig.js";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider sotre={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/store/storeConfig.js
@@ -0,0 +1,10 @@
+import { createStore, combineReducers } from "redux";
+
+const reducer = combineReducers({
+    mumeros: (state, action) => ({ min: 1, max: 10 }),
+    nomes: (state, action) => ["Paulo", "Henrique", "Silva"]
+});
+
+const store = () => createStore(reducer);
+
+export default store;
